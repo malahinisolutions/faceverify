7e25863159fa44e11f29715cc0d13e1580e92a7e
Malahini Solutions <malahinisolutions@gmail.com>
2018-09-18 19:45:46 +0530

    32

--- application/views/admin/footer.php
+++ application/views/admin/footer.php
@@ -14,8 +14,8 @@
 
 <script src="<?php echo base_url('assets/admin/js/vendor.js');?>"></script>
 <script src="<?php echo base_url('assets/admin/js/app.js');?>"></script>
-<script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
-<script src="http://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
+<script src="https://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+<script src="https://code.jquery.com/ui/1.10.3/jquery-ui.js"></script>
 
 <script>
 $("#myModal-id").draggable({
